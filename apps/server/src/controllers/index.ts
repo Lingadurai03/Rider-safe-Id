9bc1070b5ea7520b8bc3352d8e6e0039d6b3111f
Lingadurai03 <lingadurai033@gmail.com>
2025-06-17 14:39:07 +0530

    Feat: Public api to get userDetails

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { AppController } from './app.controller';
 export { AuthController } from './auth.controller';
 export { ProfileController } from './profile.controller';
+export { PublicController } from './public.controller';
--- apps/server/src/modules/app.module.ts
+++ apps/server/src/modules/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from '@/controllers';
-import { AuthModule } from '@/modules';
+import { AuthModule, PublicModule } from '@/modules';
 import { AppService } from '@/services';
 
 import { ProfileModule } from './profile.module';
 
 @Module({
-    imports: [AuthModule, ProfileModule],
+    imports: [AuthModule, ProfileModule, PublicModule],
     controllers: [AppController],
     providers: [AppService],
 })
